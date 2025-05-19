128078bf3299adacd56c498c37d5c5ac085eaa66
haoqizhang <fullyouth@163.com>
2025-05-19 22:23:29 +0800

    feat(+):默认填写目标地址

--- popup.tsx
+++ popup.tsx
@@ -26,7 +26,29 @@ function IndexPopup() {
     } else {
       console.error("chrome.storage.sync is not available")
     }
+
+    getCurrentUrl().then((url) => {
+      setAliasValue(url.toString())
+    })
   }, [])
+
+  const getCurrentUrl = () => {
+    return new Promise((resolve) => {
+      if (chrome?.tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          const currentTab = tabs[0]
+          if (currentTab) {
+            const url = new URL(currentTab.url || "")
+            resolve(url)
+          } else {
+            resolve('')
+          }
+        })
+      } else {
+        resolve('')
+      }
+    })
+  }
 
   // Save aliases to storage
   const saveAliases = (newAliases: { [key: string]: { value: string; sort: number } }, cb: () => void) => {
@@ -44,10 +66,8 @@ function IndexPopup() {
     setAliasKey(value)
     const alias = aliases[value]
     if (alias) {
-      setAliasValue(alias.value)
       setAddOrUpdate("update")
     } else {
-      setAliasValue("")
       setAddOrUpdate("add")
     }
   }
@@ -138,10 +158,10 @@ function IndexPopup() {
           <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
             <input
               className="alias-input"
-              placeholder="别名"
               value={aliasKey}
               onChange={(e) => handleChange(e.target.value)}
             />
+            <p className="alias-input-label">别名</p>
             <div
               className="alias-input-tip"
               onMouseEnter={handleTooltipMouseEnter}
@@ -150,21 +170,15 @@ function IndexPopup() {
               ?
             </div>
           </div>
-          {showTooltip && (
-              <div className="tooltip">
-                <p>1【精确匹配模式】配置：{`别名task/fat 目标https://aaa.com/task`} </p>
-                <p>task/fat  {`->`}  https://aaa.com/task</p>
-                <p>2【动态参数模式】配置：{`别名task/prod 目标https://aaa.com/task/{taskid}`}</p>
-                <p>task/prod  {`->`}  https://aaa.com/task</p>
-                <p>task/prod?taskid=111  {`->`}  https://aaa.com/task/111</p>
-                </div>
-            )}
-          <textarea
-            className="alias-input alias-input-textarea"
-            placeholder="目标地址"
-            value={aliasValue}
-            onChange={(e) => setAliasValue(e.target.value)}
-          />
+          <div style={{ position: "relative" }}>
+            <p className="alias-input-label">目标地址</p>
+            <textarea
+              className="alias-input alias-input-textarea"
+              value={aliasValue}
+              onChange={(e) => setAliasValue(e.target.value)}
+            />
+          </div>
+
         </div>
         <div>
           <button className="add-btn" onClick={handleAddOrUpdate} >
@@ -203,6 +217,47 @@ function IndexPopup() {
         </ul>
       )}
       {toastMessage && <div className="toast">{toastMessage}</div>}
+      {showTooltip && (
+        <div className="tooltip">
+          <p className="tooltip-title">1、精确匹配模式 </p>
+          <p>配置：{`别名task/fat 目标https://aaa.com/task`}</p>
+          <table className="tooltip-table">
+            <thead>
+              <tr>
+                <th>搜索栏输入</th>
+                <th>跳转地址</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>task/fat</td>
+                <td>https://aaa.com/task</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <p className="tooltip-title">2、动态参数模式</p>
+          <p>配置：{`别名task/prod 目标https://aaa.com/task/{taskid}`}</p>
+          <table className="tooltip-table">
+            <thead>
+              <tr>
+                <th>搜索栏输入</th>
+                <th>跳转地址</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>task/prod</td>
+                <td>https://aaa.com/task</td>
+              </tr>
+              <tr>
+                <td>task/prod?taskid=111</td>
+                <td>https://aaa.com/task/111</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   )
 }
